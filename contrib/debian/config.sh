a910dcc4217e9b5381097ac85ebfe012cdc8c26e
Warwick McNaughton <warwick.mcnaughton@gmail.com>
2025-07-11 15:14:15 +1200

    change

--- contrib/debian/config.sh
+++ contrib/debian/config.sh
@@ -1,5 +1,7 @@
 : ${STABLE_MIRROR:="http://deb.debian.org"}
 : ${STABLE_SUITE:="bookworm"}
-: ${BACKPORTS_MIRROR:="http://ocaml.debian.net/backports/20250313"}
-: ${BACKPORTS_SUITE:="bookworm-backports-ocaml"}
+# : ${BACKPORTS_MIRROR:="http://ocaml.debian.net/backports/20250313"}
+# : ${BACKPORTS_SUITE:="bookworm-backports-ocaml"}
+: ${BACKPORTS_MIRROR:="http://deb.debian.org"}
+: ${BACKPORTS_SUITE:="bookworm"}
 : ${KEYRING:="$(dirname "$0")/ocaml-backports-keyring.asc"}
